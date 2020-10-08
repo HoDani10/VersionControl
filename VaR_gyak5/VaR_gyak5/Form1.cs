ba262e0e1821a775e3b60db4a92e89395778119e
Dániel Horváth <daniel.horvath4@stud.uni-corvinus.hu>
2020-10-08 14:42:28 +0200

    7. feladat

--- VaR_gyak5/VaR_gyak5/Form1.cs
+++ VaR_gyak5/VaR_gyak5/Form1.cs
@@ -32,5 +32,21 @@ namespace VaR_gyak5
 
             dataGridView2.DataSource = Portfolio;
         }
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
+
+        }
+
     }
 }
