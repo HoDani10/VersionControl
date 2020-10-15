8ae2c40d1352527521da2db562c02fb936bc3945
Dániel Horváth <daniel.horvath4@stud.uni-corvinus.hu>
2020-10-15 16:35:24 +0200

    3. feladat

--- gyak6/gyak6/Form1.cs
+++ gyak6/gyak6/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using gyak6.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,28 @@ namespace gyak6
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+            arfolyam();
+
+ 
+        }
+
+        private void arfolyam()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
