f2de7c34da5228e1cec9fdf905b9aac6e67fa819
Dániel Horváth <daniel.horvath4@stud.uni-corvinus.hu>
2020-10-15 17:00:31 +0200

    6. feladat

--- gyak6/gyak6/Form1.cs
+++ gyak6/gyak6/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace gyak6
@@ -21,14 +22,17 @@ namespace gyak6
         {
             InitializeComponent();
             arfolyam();
-            dataGridView1.DataSource = Rates;
+            
+            
 
 
  
         }
 
         private void arfolyam()
+
         {
+            dataGridView1.DataSource = Rates;
             var mnbService = new MNBArfolyamServiceSoapClient();
 
             var request = new GetExchangeRatesRequestBody()
@@ -62,7 +66,23 @@ namespace gyak6
                 var value = decimal.Parse(childElement.InnerText);
                 if (unit != 0)
                     rate.Value = value / unit;
+
             }
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
     }
 }
