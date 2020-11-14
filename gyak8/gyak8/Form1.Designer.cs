0b6a3730a557bf8794b485744da1145b1820791c
Dániel Horváth <daniel.horvath4@stud.uni-corvinus.hu>
2020-11-14 21:04:55 +0100

    11. feladat

--- gyak8/gyak8/Form1.Designer.cs
+++ gyak8/gyak8/Form1.Designer.cs
@@ -32,11 +32,18 @@
             this.mainPanel = new System.Windows.Forms.Panel();
             this.createTimer = new System.Windows.Forms.Timer(this.components);
             this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.buttoncar = new System.Windows.Forms.Button();
+            this.buttonball = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mainPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainPanel
             // 
             this.mainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainPanel.Controls.Add(this.label1);
+            this.mainPanel.Controls.Add(this.buttonball);
+            this.mainPanel.Controls.Add(this.buttoncar);
             this.mainPanel.Location = new System.Drawing.Point(12, 34);
             this.mainPanel.Name = "mainPanel";
             this.mainPanel.Size = new System.Drawing.Size(624, 370);
@@ -54,6 +61,35 @@
             this.conveyorTimer.Interval = 10;
             this.conveyorTimer.Tick += new System.EventHandler(this.conveyorTimer_Tick);
             // 
+            // buttoncar
+            // 
+            this.buttoncar.Location = new System.Drawing.Point(189, 19);
+            this.buttoncar.Name = "buttoncar";
+            this.buttoncar.Size = new System.Drawing.Size(68, 67);
+            this.buttoncar.TabIndex = 0;
+            this.buttoncar.Text = "Car";
+            this.buttoncar.UseVisualStyleBackColor = true;
+            this.buttoncar.Click += new System.EventHandler(this.buttoncar_Click);
+            // 
+            // buttonball
+            // 
+            this.buttonball.Location = new System.Drawing.Point(276, 19);
+            this.buttonball.Name = "buttonball";
+            this.buttonball.Size = new System.Drawing.Size(68, 67);
+            this.buttonball.TabIndex = 1;
+            this.buttonball.Text = "Ball";
+            this.buttonball.UseVisualStyleBackColor = true;
+            this.buttonball.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(371, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Coming next:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -62,6 +98,8 @@
             this.Controls.Add(this.mainPanel);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -71,6 +109,9 @@
         private System.Windows.Forms.Panel mainPanel;
         private System.Windows.Forms.Timer createTimer;
         private System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonball;
+        private System.Windows.Forms.Button buttoncar;
     }
 }
 
--- gyak8/gyak8/Form1.cs
+++ gyak8/gyak8/Form1.cs
@@ -16,12 +16,18 @@ namespace gyak8
     {
         private List<Toy> _toys = new List<Toy>();
 
+        private Toy _nextToy;
+
         private IToyFactory _factory;
 
         public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set 
+            {
+            _factory = value;
+            DisplayNext();
+            }
         }
 
         public Form1()
@@ -57,5 +63,26 @@ namespace gyak8
 
             }
         }
-    }
+
+        private void buttoncar_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void buttonball_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
+        }
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+            Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = label1.Top + label1.Height + 20;
+            _nextToy.Left = label1.Left;
+            Controls.Add(_nextToy);
+
+
+
+        }
 }
