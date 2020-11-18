db0accd4a67a6d5412727f6cf7b0fd35ca30aef8
Dániel Horváth <daniel.horvath4@stud.uni-corvinus.hu>
2020-11-18 14:56:00 +0100

    3. feladat

---
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestExample.Test
+{
+    class AccountControllerTestFixture
+    {
+        public void TestValidateEmail(string email, bool expectedResult)
+        { 
+        }
+    }
+}
