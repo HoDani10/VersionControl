29d8c3c6d6efa0921b188afb3b947a591a735cd0
Dániel Horváth <daniel.horvath4@stud.uni-corvinus.hu>
2020-11-18 15:01:09 +0100

    5. feladat

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -1,15 +1,25 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestExample.Controllers;
 
 namespace UnitTestExample.Test
 {
     class AccountControllerTestFixture
     {
         public void TestValidateEmail(string email, bool expectedResult)
-        { 
+        {
+            //arrange
+            var accountController = new AccountController();
+
+            //act
+            var actualResult = accountController.ValidateEmail(email);
+
+            //assert
+            Assert.AreEqual(expectedResult, actualResult);
         }
     }
 }
