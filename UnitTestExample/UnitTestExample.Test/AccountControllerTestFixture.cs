fe7f4742806a8bcdf80a5394c456541323486554
Dániel Horváth <daniel.horvath4@stud.uni-corvinus.hu>
2020-11-18 15:09:45 +0100

    6. feladat

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,12 @@ namespace UnitTestExample.Test
 {
     class AccountControllerTestFixture
     {
+        [
+            Test,
+            TestCase("abcd1234", false),
+            TestCase("irf@uni-corvinus", false),
+            TestCase("irf.uni-corvinus.hu", false),
+            TestCase("irf@uni-corvinus.hu", true)]
         public void TestValidateEmail(string email, bool expectedResult)
         {
             //arrange
